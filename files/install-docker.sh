d0c6b9b7faab22369c5ec9a1cc7dfbf88f9bc0ac
groldo <grodlo@dumpme.de>
2020-03-31 09:03:05 -0400

    removed install-docker.sh in favor of ansible tasks

--- files/install-docker.sh
+++
@@ -1,2 +0,0 @@
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo echo 'deb [arch=amd64] https://download.docker.com/linux/debian buster stable' | sudo tee /etc/apt/sources.list.d/docker.list
